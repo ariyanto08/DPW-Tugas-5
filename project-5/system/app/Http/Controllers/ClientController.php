7d1a7f09b09a1764dd8bde7490e7ff55d56bbec8
ariyanto08 <102232039+ariyanto08@users.noreply.github.com>
2022-09-04 12:07:10 +0700

    upload

---
+++ project-5/system/app/Http/Controllers/ClientController.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Controllers;
+use App\Models\Product;
+
+class ClientController extends Controller{
+
+    function showHome(){
+        $data['list_product'] = Product::all();
+        return view('template.home',$data);
+    }
+    function showShop(){
+        $data['list_product'] = Product::all();
+        return view('template.shop',$data);
+    }
+
+    function showDetail(Product $product){
+        $data['product'] = $product;
+        return view('template.produk',$data);
+    }
+
+    function showModal(Product $product){
+        $data['product'] = $product;
+        return view('template.modal1',$data);
+    }
+    function showCart(){
+        return view('template.cart');
+    }
+    function showAbout(){
+        return view('template.about');
+    }
+    function showContact(){
+        return view('template.contact');
+    }
+    function showShoping(){
+        return view('template.shoping');
+    }
+//     function showProduk(Product $product){
+//         $data['list_product'] = $product;
+//     return view('template.produk', $data);
+//   }
+}
